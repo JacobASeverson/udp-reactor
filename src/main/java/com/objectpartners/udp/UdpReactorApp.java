e10f3e39bcf38438e5c1063691c14a227d3493d0
jacobaseverson <jacoba.severson@gmail.com>
2014-11-06 18:16:31 -0600

    initial commit with the app pretty much done

---
+++ src/main/java/com/objectpartners/udp/UdpReactorApp.java
@@ -0,0 +1,53 @@
+package com.objectpartners.udp;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.Environment;
+import reactor.io.encoding.StandardCodecs;
+import reactor.net.netty.udp.NettyDatagramServer;
+import reactor.net.tcp.support.SocketUtils;
+import reactor.net.udp.DatagramServer;
+import reactor.net.udp.spec.DatagramServerSpec;
+import reactor.spring.context.config.EnableReactor;
+
+import java.util.concurrent.CountDownLatch;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableReactor
+@ComponentScan
+public class UdpReactorApp {
+
+    private Log log = LogFactory.getLog(UdpReactorApp.class);
+
+    @Bean
+    public DatagramServer<byte[], byte[]> datagramServer(Environment env) {
+
+        final DatagramServer<byte[], byte[]> server = new DatagramServerSpec<byte[], byte[]>(NettyDatagramServer.class)
+                .env(env)
+                .listen(SocketUtils.findAvailableTcpPort())
+                .codec(StandardCodecs.BYTE_ARRAY_CODEC)
+                .consumeInput(bytes -> log.info("received a packet: " + new String(bytes)))
+                .get();
+
+        server.start();
+        return server;
+    }
+
+    @Bean
+    public CountDownLatch latch() {
+        return new CountDownLatch(1);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ApplicationContext ctx = SpringApplication.run(UdpReactorApp.class);
+        CountDownLatch latch = ctx.getBean(CountDownLatch.class);
+        latch.await();
+    }
+}
